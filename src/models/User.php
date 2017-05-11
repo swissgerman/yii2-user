321f903129a6ae95d03223c7c0d612ddb276fe7f
Uncle Sam <samuel@kochmail.com>
2017-05-11 14:56:52 +0200

    Update User.php

--- src/models/User.php
+++ src/models/User.php
@@ -36,7 +36,8 @@ class User extends ActiveRecord implements IdentityInterface
     const STATUS_DELETED = 0;
     const STATUS_ACTIVE = 10;
 
-    const USERGROUP_ADMIN = 1;
+    const USERGROUP_ADMIN = 10;
+    const USERGROUP_USER = 20;
 
     /**
      * @inheritdoc
