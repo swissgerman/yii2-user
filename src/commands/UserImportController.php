3724feb654cfee45dcb281fbeea90aa8de8633b5
Uncle Sam <samuel@kochmail.com>
2017-05-11 14:56:06 +0200

    Update UserImportController.php

--- src/commands/UserImportController.php
+++ src/commands/UserImportController.php
@@ -13,8 +13,9 @@ use samkoch\yii2user\models\User;
  */
 class UserImportController extends Controller
 {
-
     const LOG_TARGET = 'import';
+
+    public $importedObjectGuids = [];
 
     /**
      * Retrieve users from Active Directory
@@ -22,95 +23,102 @@ class UserImportController extends Controller
      */
     public function actionIndex()
     {
-        if (is_array(Yii::$app->params['userGroups']) && count(Yii::$app->params['userGroups'])) {
-            $importedObjectGuids = [];
+        if (is_array(Yii::$app->params['userGroups'])) {
+            foreach (Yii::$app->params['userGroups'] as $role => $adGroup) {
+                $adUsers = $this->retrieveUsersFromDirectory($adGroup);
 
-            foreach (Yii::$app->params['userGroups'] as $adGroup) {
-                $filter = '(&(objectClass=Group)(CN=' . $adGroup . '))';
-                $groupAttributes = array('objectGUID', 'displayName', 'distinguishedName', 'managedBy');
-                $userAttributes = array('objectGUID', 'displayName', 'distinguishedName', 'mail', 'sAMAccountName', 'sn', 'givenName');
-                $rangeAttributes = array('member');
-                $memberFilterGroups = array(
-                    array(
-                        'type' => 'AND',
-                        'value' => 'OU=Groups',
-                    ),
-                    array(
-                        'type' => 'OR',
-                        'value' => array(
-                            'CN=ARG_',
-                            'CN=ORG_',
-                            'CN=PRO_',
-                        ),
-                    ),
-                );
-                $memberFilterUser = array(
-                    array(
-                        'value' => 'OU=Users',
-                    ),
-                );
-
-                Yii::info('Retrieving users from Active Directory.', self::LOG_TARGET);
-
-                //get LDAP component
-                $ldap = yii::$app->ldap;
-
-                $adUsers = $ldap->getResolvedUsers($filter, $groupAttributes,
-                    $rangeAttributes, $userAttributes, $memberFilterGroups, $memberFilterUser);
-
-                if ($adUsers && count($adUsers)) {
-                    foreach ($adUsers as $adUser) {
-
-                        $adUser['displayName'] = utf8_encode($adUser['displayName']);
-
-                        $user = User::find()->where(['object_guid' => $adUser['objectGUID']])->one();
-                        if (!$user) {
-                            $user = new User;
-                            $user->object_guid = $adUser['objectGUID'];
-
-                            Yii::info('Adding new user ' . $adUser['displayName'] . '.', self::LOG_TARGET);
-                        } else {
-                            Yii::info('Updating user ' . $adUser['displayName'] . '.', self::LOG_TARGET);
-                        }
-
-                        $user->email = $adUser['mail'];
-                        $user->display_name = $adUser['displayName'];
-                        $user->username = $adUser['sAMAccountName'];
-                        $user->first_name = $adUser['givenName'];
-                        $user->last_name = $adUser['sn'];
-                        $user->status = User::STATUS_ACTIVE;
-
-                        if (count($user->getMemberOfAdAsArray())) {
-                            $user->setMemberOfAdFromArray(array_merge($user->getMemberOfAdAsArray(), [$adGroup]));
-                        } else {
-                            $user->setMemberOfAdFromArray([$adGroup]);
-                        }
-
-                        $user->save();
-
-                        $importedObjectGuids[] = $adUser['objectGUID'];
-                    }
+                foreach ($adUsers as $adUser) {
+                    $this->importUser($adUser, $adGroup, $role === 'admin' ? true : false);
                 }
-
             }
 
-            //delete not imported users, except admin and local users
-            $usersToBeDeleted = User::find()
-                ->where(['!=', 'user_group_id', User::USERGROUP_ADMIN])
-                ->andWhere(['!=', 'local_user', 1])
-                ->andWhere(['NOT IN', 'object_guid', $importedObjectGuids])
-                ->andWhere(['!=', 'status', User::STATUS_DELETED])
-                ->all();
+            $this->cleanUpUsers();
+        }
+    }
 
-            Yii::info('Deleted ' . count($usersToBeDeleted) . ' obsolete users.', self::LOG_TARGET);
+    public function retrieveUsersFromDirectory($group)
+    {
+        $filter = '(&(objectClass=Group)(CN=' . $group . '))';
+        $groupAttributes = ['objectGUID', 'displayName', 'distinguishedName', 'managedBy'];
+        $userAttributes = ['objectGUID', 'displayName', 'distinguishedName', 'mail', 'sAMAccountName', 'sn', 'givenName'];
+        $rangeAttributes = ['member'];
+        $memberFilterGroups = [
+            [
+                'type' => 'AND',
+                'value' => 'OU=Groups',
+            ],
+            [
+                'type' => 'OR',
+                'value' => [
+                    'CN=ARG_',
+                    'CN=ORG_',
+                    'CN=PRO_',
+                ],
+            ],
+        ];
+        $memberFilterUser = [['value' => 'OU=Users']];
 
-            foreach ($usersToBeDeleted as $userToBeDeleted) {
-                $userToBeDeleted->status = User::STATUS_DELETED;
-                $userToBeDeleted->save(false);
-            }
+        Yii::info('Retrieving users from directory.', self::LOG_TARGET);
 
+        //get LDAP component
+        $ldap = yii::$app->ldap;
+
+        return $ldap->getResolvedUsers($filter, $groupAttributes, $rangeAttributes, $userAttributes, $memberFilterGroups, $memberFilterUser);
+    }
+
+    public function importUser($adUser, $adGroup, $admin = false)
+    {
+        $adUser['displayName'] = utf8_encode($adUser['displayName']);
+
+        $user = User::find()->where(['object_guid' => $adUser['objectGUID']])->one();
+        if (!$user) {
+            $user = new User;
+            $user->object_guid = $adUser['objectGUID'];
+
+            Yii::info('Added new user ' . $adUser['displayName'] . '.', self::LOG_TARGET);
+        } else {
+            Yii::info('Updated user ' . $adUser['displayName'] . '.', self::LOG_TARGET);
         }
 
+        $user->email = $adUser['mail'];
+        $user->display_name = $adUser['displayName'];
+        $user->username = $adUser['sAMAccountName'];
+        $user->first_name = $adUser['givenName'];
+        $user->last_name = $adUser['sn'];
+        $user->status = User::STATUS_ACTIVE;
+
+        $user->user_group_id = User::USERGROUP_USER;
+        if($admin === true) {
+            $user->user_group_id = User::USERGROUP_ADMIN;
+        }
+
+        if (count($user->getMemberOfAdAsArray())) {
+            $user->setMemberOfAdFromArray(array_merge($user->getMemberOfAdAsArray(), [$adGroup]));
+        } else {
+            $user->setMemberOfAdFromArray([$adGroup]);
+        }
+
+        $this->importedObjectGuids[] = $adUser['objectGUID'];
+
+        return $user->save();
+    }
+
+    public function cleanUpUsers()
+    {
+        //delete not imported users, except admin and local users
+        $usersToBeDeleted = User::find()
+            ->where(['!=', 'user_group_id', User::USERGROUP_ADMIN])
+            ->andWhere(['!=', 'local_user', 1])
+            ->andWhere(['NOT IN', 'object_guid', $this->importedObjectGuids])
+            ->andWhere(['!=', 'status', User::STATUS_DELETED])
+            ->all();
+
+        Yii::info('Deleted ' . count($usersToBeDeleted) . ' obsolete users.', self::LOG_TARGET);
+
+        foreach ($usersToBeDeleted as $userToBeDeleted) {
+            $userToBeDeleted->status = User::STATUS_DELETED;
+            $userToBeDeleted->save(false);
+        }
     }
 
 }
